a555e986d1166a32adc6691e65c000cbb1b50f3c
abdelazizhanafi <abdelazizhaanafy@gmail.com>
2026-04-01 11:19:50 +0200

    feat: Add splash and onboarding views with navigation
    - Implemented SplashView and OnBoardingView as separate screens.
    - Created a navigation helper to manage route generation.
    - Added SVG assets for splash screen visuals.
    - Developed SplashViewBody to handle the splash screen layout and navigation logic.
    - Defined asset paths in a centralized Assets class for better management.

---
+++ lib/core/helper/on_generate_routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub_app/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:fruit_hub_app/features/splash/presentation/views/splash_view.dart';
+
+Route<dynamic> onGenerateRoutes(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (_) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (_) => const OnBoardingView());
+    default:
+      return MaterialPageRoute(builder: (_) => const Scaffold());
+  }
+}
